2a2aa88f26103b4902f52a471075a816714f4fb1
RicardoFV99 <ricardofv291099@gmail.com>
2021-11-03 14:55:36 +0000

    Se comenta la app

--- food_management/settings.py
+++ food_management/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'food_management_app',
+    #'food_management_app',
 ]
 
 MIDDLEWARE = [
